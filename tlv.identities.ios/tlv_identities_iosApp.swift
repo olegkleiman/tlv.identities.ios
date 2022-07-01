579d80bf8e40bcb89796eeab410ab33f765891f4
Oleg Kleiman <oleg_kleyman@hotmail.com>
2022-07-02 01:22:44 +0300

    Initial Commit

---
+++ tlv.identities.ios/tlv_identities_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tlv_identities_iosApp.swift
+//  tlv.identities.ios
+//
+//  Created by Oleg Kleiman on 02/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct tlv_identities_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
